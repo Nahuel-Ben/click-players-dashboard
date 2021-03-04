7e8c06ebb2859ae2f326121ffdcdd025388873d1
Nahuel-Ben <nahuel.b13@gmail.com>
2021-03-04 11:26:14 -0300

    edit components to class

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,53 +1,49 @@
-import {React} from 'react';
+import React, {Component} from 'react';
 
-function Product (props) {
+class Product extends React.Component {
+
+    constructor(props) {
+        super(props)
+            this.state = {
+                meta: {},
+                data: []
+            }
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:4000/api/products")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    meta: result.meta,
+                    products: result.data
+                    });
+            },
+            (error) => {
+                this.setState({
+                    error
+                });
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <p>{this.state.data.name} | {this.state.data.price} | {this.state.data.discount}</p>
+            </div>
+        )
+
+    }
+}
+/* function Product (props) {
     return (
         <div>
             <p>{props.name} | {props.price} | {props.discount}</p>
         </div>
     )
-}
-
-/* class Product extends Component {
-
-    constructor (props) {
-        super (props);
-        this.state = {
-            isLoaded: false,
-            products: []
-        }
-    }
-
-    componentDidMount() {
-            fetch("http://localhost:4000/api/products")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        products: result.products
-                        });
-                },
-                (error) => {
-                    this.setState({
-                        error
-                    });
-                }
-            )
-    }
-
-    render () {
-            const {error, products} = this.state;
-            if(error) {
-                return <div>Error: {error.message}</div>;
-            /* }else if (!isLoaded){
-                return <div>Cargando...</div> */
-            /*}else {
-                return (
-                    {products}
-                );
-            }            
-    }
 } */
+
 
 export default Product;
--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,40 @@
-import {React} from 'react';
+import React, {Component} from 'react';
 
-function User (props) {
-    return (
-        <div>
-            <p>{props.name} | {props.email}</p>
-        </div>
-    )
+class User extends React.Component {
+
+    constructor(props) {
+        super(props)
+            this.state = {
+                meta: {},
+                data: []
+            }
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:4000/api/users")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    meta: result.meta,
+                    users: result.data
+                    });
+            },
+            (error) => {
+                this.setState({
+                    error
+                });
+            }
+        )
+    }
+
+    render(){
+        return (
+            <div>
+                <p>{this.state.data.first_name} | {this.state.data.email}</p>
+            </div>
+        )
+    }
 }
 
 
